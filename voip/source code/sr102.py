c4409b8c36ac93005b92755cc9e022c37097e747
jarvi652 <jarviskatana@gmail.com>
2020-11-09 14:35:43 +0300

    final draft

--- voip/source code/sr102.py
+++ voip/source code/sr102.py
@@ -10,9 +10,44 @@ from json import loads
 
 
 class Server:
+    def handle_client(self, conn, addr):
+        while 1:
+            try:
+                data = conn.recv(1024)
+                chunk_size = 1024
+                audio_format = pyaudio.paInt16
+                channels = 1
+                rate = 20000
+                seconds = 5
+                frames = []
+                filename = "myfile.wav"
+                p = pyaudio.PyAudio()
+                play_stream = p.open(format=audio_format, channels=channels, rate=rate, output=True,
+                                     frames_per_buffer=chunk_size)
+                i = 1
+                while data != '':
+                    play_stream.write(data)
+                    data = conn.recv(1024)
+                    i = i + 1
+                    frames.append(data)
+                wf = wave.open(filename, "wb")
+                wf.setnchannels(channels)
+                wf.setsampwidth(p.get_sample_size(audio_format))
+                wf.setframerate(rate)
+                wf.writeframes(b''.join(frames))
+                wf.close()
+                play_stream.stop_stream()
+                play_stream.close()
+                p.terminate()
+                self.broadcast(conn, data)
+            except socket.error:
+                conn.close()
+                print("Server session interrupted")
+                self.__class__()
     def __init__(self):
         self.ip = socket.gethostbyname(socket.gethostname())
         self.port = int(input('Enter port number to run on --> '))
+        self.address = input('enter  Computer-name -->')
         print("Server ip:" + self.ip)
         while 1:
             try:
@@ -33,7 +68,6 @@ class Server:
             conn, addr = self.s.accept()
             print("Client: " + str(addr[0]) + " connected")
             self.connections.append(conn)
-
             threading.Thread(target=self.handle_client, args=(conn, addr)).start()
 
     def broadcast(self, sock, data):
@@ -42,43 +76,9 @@ class Server:
                 try:
                     client.send(data)
                 except:
-                    Server()
+                    self.__class__()
 
-    def handle_client(self, conn, addr):
-        while 1:
-            try:
-                data = conn.recv(1024)
-                chunk_size = 1024
-                audio_format = pyaudio.paInt16
-                channels = 1
-                rate = 20000
-                seconds = 5
-                frames = []
-                filename = "myfile.wav"
-                p = pyaudio.PyAudio()
-                play_stream = p.open(format=audio_format, channels=channels, rate=rate, output=True,
-                                     frames_per_buffer=chunk_size)
-                i = 1
-                while data != '':
-                    play_stream.write(data)
-                    data = conn.recv(1024)
-                    i = i + 1
-                    print(i)
-                    frames.append(data)
-                wf = wave.open(filename, "wb")
-                wf.setnchannels(channels)
-                wf.setsampwidth(p.get_sample_size(audio_format))
-                wf.setframerate(rate)
-                wf.writeframes(b''.join(frames))
-                wf.close()
-                play_stream.stop_stream()
-                play_stream.close()
-                p.terminate()
-                self.broadcast(conn, data)
-            except socket.error:
-                conn.close()
-                print("Server session interrupted")
-                Server()
+
 
 
 server = Server()
